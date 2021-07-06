5f91d33db6cedd06237bbb0830c0fb4da7f4b003
Alex Tkachenko <skyalexx@gmail.com>
2021-07-06 21:21:13 +0300

    Support multiple pairs

--- scripts/trade.js
+++ scripts/trade.js
@@ -8,9 +8,11 @@ const { logBlock, createTrade, flashswapProfit } = require("./trade-utils")
 
 // deployed FlashSwap.sol contract
 const flashswapAddress = '0x70e0bA845a1A0F2DA3359C97E0285013525FFC49'
-const tokenAAddress = cfg.DAI
-const tokenBAddress = cfg.WETH
-
+const routeAddresses = [
+    [cfg.DAI, cfg.WETH],
+    [cfg.USDC, cfg.WETH],
+    [cfg.WETH, cfg.LINK]
+]
 const dex0 = {
     name: "Uniswap",
     factory: cfg.uni.factory,
@@ -21,6 +23,9 @@ const dex1 = {
     factory: cfg.sushi.factory,
     router: cfg.sushi.router
 }
+
+var senderAddress
+var isSwapping = false
 
 
 // Flashswaps by borrowing amountBorrow of borrowTokenAddress on dex0, 
@@ -33,8 +38,7 @@ async function flashswap(pair, borrowTokenAddress, amountBorrow) {
 
     try {
         console.log(`\n[flashswap] executing flashswap...`)
-        const tx = await pair.swap(amount0, amount1, flashswapAddress, data)
-        const receipt = await tx.wait()
+        const receipt = await (await pair.swap(amount0, amount1, flashswapAddress, data)).wait()
         console.log(`[flashswap] flashswap success: blockNumber = ${receipt.blockNumber}, status = ${receipt.status}, tx = ${receipt.transactionHash}, gasUsed = ${receipt.gasUsed.toString()}\n`)
         return true
     } catch (error) {
@@ -43,13 +47,36 @@ async function flashswap(pair, borrowTokenAddress, amountBorrow) {
     }
 }
 
-async function main() {
-    const senderAddress = await (await ethers.provider.getSigner()).getAddress()
+async function checkRoute(blockNumber, route) {
+    const reserves0 = await route.pair0.getReserves()
+    const reserves1 = await route.pair1.getReserves()
+    
+    // calc how much token should be borrowed to balance prices between two DEXes
+    const trade = createTrade(reserves0, reserves1)
+    const amountBorrow = trade.amountBorrowA ? trade.amountBorrowA : trade.amountBorrowB
+    const tokenBorrowInfo = trade.amountBorrowA ? route.tokenAInfo : route.tokenBInfo
+    
+    // calculate max potential profit by flashswapping amountBorrow tokens
+    const maxProfit = flashswapProfit(reserves0, reserves1, trade.amountBorrowA, trade.amountBorrowB)
+    
+    logBlock(blockNumber, dex0, dex1, route.tokenAInfo, route.tokenBInfo, reserves0, reserves1, tokenBorrowInfo, amountBorrow, maxProfit, true)
+    
+    // swap if Uniswap price is higher than Sushiswap and there is an opportunity for profit
+    if (maxProfit.gte(0) && !isSwapping) {
+        isSwapping = true
+        const success = await flashswap(route.pair0, tokenBorrowInfo.address, amountBorrow)
+        isSwapping = false
+        if (success) {
+            await logBalance(senderAddress, route.tokenAInfo.address, route.tokenBInfo.address)
+        }
+    }      
+}
 
+async function buildRoute(tokenAAddress, tokenBAddress) {
     const pair0 = await getPairContract(dex0.factory, tokenAAddress, tokenBAddress)
     const pair1 = await getPairContract(dex1.factory, tokenAAddress, tokenBAddress)
     if (pair0.address == 0 || pair1.address == 0) {
-        console.error('No pairs for given tokens')
+        console.error(`No pair for route [${tokenAAddress}, ${tokenBAddress}]: dex0 = ${pair0.address}, dex1 = ${pair1.address}`)
         process.exit(1)
     }
 
@@ -57,35 +84,29 @@ async function main() {
     const tokenAInfo = await tokenInfo(await pair0.token0())
     const tokenBInfo = await tokenInfo(await pair0.token1())
 
-    var isSwapping = false
+    return {
+        tokenAInfo: tokenAInfo,
+        tokenBInfo: tokenBInfo,
+        pair0: pair0,
+        pair1: pair1
+    }
+}
 
-    var checkPrices = async function (blockNumber) {
-        if (isSwapping) return
-    
-        const reserves0 = await pair0.getReserves()
-        const reserves1 = await pair1.getReserves()
-        
-        // calc how much token should be borrowed to balance prices between two DEXes
-        const trade = createTrade(reserves0, reserves1)
-        const amountBorrow = trade.amountBorrowA ? trade.amountBorrowA : trade.amountBorrowB
-        const tokenBorrowInfo = trade.amountBorrowA ? tokenAInfo : tokenBInfo
-        // calculate max potential profit by flashswapping amountBorrow tokens
-        const maxProfit = flashswapProfit(reserves0, reserves1, trade.amountBorrowA, trade.amountBorrowB)
-        
-        logBlock(blockNumber, dex0, dex1, tokenAInfo, tokenBInfo, reserves0, reserves1, tokenBorrowInfo, amountBorrow, maxProfit, true)
-        // swap if Uniswap price is higher than Sushiswap and there is an opportunity for profit
-        if (maxProfit.gte(0)) {
-            isSwapping = true
-            const success = await flashswap(pair0, tokenBorrowInfo.address, amountBorrow)
-            if (success) {
-                await logBalance(senderAddress, tokenAInfo.address, tokenBInfo.address)
-                process.exit(0) // turn the app off on successs
-            }
-            isSwapping = false
-        }        
+
+async function main() {
+    senderAddress = await (await ethers.provider.getSigner()).getAddress()
+
+    const routes = []
+    for (var i = 0; i < routeAddresses.length; i++) {
+        const [tokenAAddress, tokenBAddress] = routeAddresses[i]
+        const route = await buildRoute(tokenAAddress, tokenBAddress)
+        routes.push(route)
+        console.log(`route[${i}]: [${route.tokenAInfo.symbol}/${route.tokenBInfo.symbol}]`)
     }
 
-    ethers.provider.on('block', checkPrices)
+    ethers.provider.on('block', async function (blockNumber) {
+        routes.forEach(route => checkRoute(blockNumber, route)) 
+    })
 }
 
 runApp(main)
