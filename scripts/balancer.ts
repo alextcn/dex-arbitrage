0e61e272eeab43d93ae63f78f29aafbaadd1fcc2
Alex Tkachenko <skyalexx@gmail.com>
2021-07-15 18:13:24 +0300

    Calc output for given input

--- scripts/balancer.ts
+++ scripts/balancer.ts
@@ -3,18 +3,20 @@ import { ethers } from "hardhat"
 import { runScript } from "./utils"
 import cfg from '../config.json'
 import { BigNumber } from "ethers"
+import { bmath } from "@balancer-labs/sor"
+import { BigNumber as BN } from "./bignumber"
 
 const valutAddress = '0xBA12222222228d8Ba445958a75a0704d566BF2C8'
 const poolID = '0x0b09dea16768f0799065c475be02919503cb2a3500020000000000000000001a' // 60WETH-40DAI
 
 
-runScript(async function() {
+runScript(async function () {
     const vaultAbi = [
         'function getPool(bytes32 poolId) external view returns (address, uint8)',
         'function getPoolTokens(bytes32 poolId) external view returns (address[] memory tokens, uint256[] memory balances, uint256 lastChangeBlock)'
     ]
     const vault = await ethers.getContractAt(vaultAbi, valutAddress)
-    
+
     const [poolAddress] = await vault.getPool(poolID)
     console.log(`vault.getPool: poolAddress = ${poolAddress}`)
 
@@ -27,13 +29,15 @@ runScript(async function() {
         "function name() view returns (string)",
         "function symbol() view returns (string)",
         "function getNormalizedWeights() external view returns (uint256[] memory)",
+        "function getSwapFeePercentage() external view returns (uint256)"
     ]
     const pool = await ethers.getContractAt(poolAbi, poolAddress)
 
     const poolName = await pool.name()
     const [daiWeight, wethWeight] = (await pool.getNormalizedWeights()) as BigNumber[] // 18 decimals: [0.6, 0.4]
-    console.log(`pool: name = ${poolName}, weights = [${ethers.utils.formatUnits(daiWeight, 16)}%, ${ethers.utils.formatUnits(wethWeight, 16)}%]`)
-    
+    const swapFee = (await pool.getSwapFeePercentage()) as BigNumber
+    console.log(`pool: name = ${poolName}, swapFee = ${ethers.utils.formatUnits(swapFee, 18)}, weights = [${ethers.utils.formatUnits(daiWeight, 16)}%, ${ethers.utils.formatUnits(wethWeight, 16)}%]`)
+
     // 1 WETH = (DAI/40)/(WETH/60)
     const precision = 18
     const wethPrice = BigNumber.from(10).pow(precision).mul(daiBalance).div(daiWeight).div(wethBalance.div(wethWeight))
@@ -41,4 +45,18 @@ runScript(async function() {
     console.log(`1 WETH = ${ethers.utils.formatUnits(wethPrice, precision)} DAI`)
     console.log(`1 DAI  = ${ethers.utils.formatUnits(daiPrice, precision)} WETH`)
 
+    // calculate price after selling/buing 10 WETH
+    const amountIn = ethers.utils.parseUnits('10', 18)    
+    const amountOutBN = bmath.calcOutGivenIn(toBN(wethBalance), toBN(wethWeight), toBN(daiBalance), toBN(daiWeight), toBN(amountIn), toBN(swapFee))
+    const amountOut = fromBN(amountOutBN)
+    console.log(`swap ${ethers.utils.formatUnits(amountIn, 18)} WETH for ${ethers.utils.formatUnits(amountOut, 18)} DAI`)
 })
+
+
+function toBN(x: BigNumber): BN {
+    return new BN(x.toString())
+}
+
+function fromBN(x: BN): BigNumber {
+    return BigNumber.from(x.toString())
+}
